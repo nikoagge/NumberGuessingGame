2c79b92bb83bca1e20e2aaa8cb0fa79d96d227d8
nikolas <nikoagge@gmail.com>
2020-11-28 17:36:29 +0200

    NumberGuessingGame implemented. Last commit for this project.

--- NumberGuessingGame/Program.cs
+++ NumberGuessingGame/Program.cs
@@ -6,7 +6,32 @@ namespace NumberGuessingGame
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random randomInstance = new Random();
+            int winningNumber = randomInstance.Next(0, 13);
+            bool win = false;
+
+            do
+            {
+                Console.Write("Guess a number inbetween 0 and 13: ");
+                string inputNumberStringFormat = Console.ReadLine();
+                int inputNumberIntFormat = int.Parse(inputNumberStringFormat);
+
+                if (inputNumberIntFormat > winningNumber)
+                {
+                    Console.WriteLine("Too high! Guess lower...");
+                } else if (inputNumberIntFormat < winningNumber)
+                {
+                    Console.WriteLine("Go low! Guess higher...");
+                } else if (inputNumberIntFormat == winningNumber)
+                {
+                    Console.WriteLine("You won!");
+                    win = true;
+                }
+                Console.WriteLine();
+            } while (win == false);
+
+            Console.WriteLine("Thanks for playing! Press any key to finish...");
+            Console.ReadKey(true);
         }
     }
 }
