7e11b5483fa46f65ea53f3bfef9806f9d1cac502
nikolas <nikoagge@gmail.com>
2020-11-28 16:48:54 +0200

    Initial commit

---
+++ NumberGuessingGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NumberGuessingGame
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
